c326f40138101fe9627722af7fd927d6ed0a4994
shivam5620 <sbirla04082000@gmail.com>
2025-08-03 10:53:52 +0530

    fix type

--- app/_api/product.ts
+++ app/_api/product.ts
@@ -1,17 +1,17 @@
 import Ax from "@/lib/axios";
-import { Product } from "@/types/product";
-import { StrapiResponse } from "@/types/strapi";
+import { IProduct } from "@/types/product";
+import { IStrapiResponse } from "@/types/strapi";
 
-export async function fetchProducts(): Promise<Product[]> {
-  const res = await Ax.get<StrapiResponse<Product[]>>(
+export async function fetchProducts(): Promise<IProduct[]> {
+  const res = await Ax.get<IStrapiResponse<IProduct[]>>(
     "/api/products?populate=*"
   );
   return res.data.data;
 }
 export async function fetchProductByDocumentId(
   documentId: string
-): Promise<Product | null> {
-  const res = await Ax.get<StrapiResponse<Product[]>>(
+): Promise<IProduct | null> {
+  const res = await Ax.get<IStrapiResponse<IProduct[]>>(
     `/api/products?filters[documentId][$eq]=${documentId}&populate=*`
   );
   return res.data.data?.[0] || null;
