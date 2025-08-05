c16950b4b3591811eb6787ae14b947a92680fa5d
shivam5620 <sbirla04082000@gmail.com>
2025-08-05 10:16:47 +0530

    add google font

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,10 +4,23 @@ import { NavigationMenu } from "./component/navbar/Header";
 import { Toaster } from "@/components/ui/sonner";
 import { Banner } from "./component/navbar/Banner";
 import { Footer } from "./component/navbar/Footer";
+import { Inter, Philosopher } from "next/font/google";
+
 export const metadata = {
   title: "Shop~Easy",
   description: "Using TanStack Query in Next.js",
 };
+
+export const inter = Inter({
+  variable: "--font-inter",
+  subsets: ["latin"],
+});
+
+export const philosopher = Philosopher({
+  variable: "--font-philosopher",
+  subsets: ["latin"],
+  weight: ["400", "700"],
+});
 
 export default function RootLayout({
   children,
@@ -16,7 +29,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className="bg-gradient-to-r from-yellow-300 to-yellow-500 p-6 text-black rounded-lg">
+      <body
+        className={`${philosopher.className} bg-gradient-to-r from-yellow-300 to-yellow-500 p-6 text-black rounded-lg`}
+      >
         <Providers>
           <NavigationMenu />
           <Banner />
